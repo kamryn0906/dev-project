5672cd8edd85e415d9b6bfda5d78c73577f7f4ad
kamryn0906 <delaleu.k@gmail.com>
2022-04-04 10:02:42 -0400

    header updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,13 @@ export default class Header extends Component {
             <img src="https://th.bing.com/th/id/OIP.UOMpj5qSonyfY2m0DPk1uQHaEU?w=278&h=180&c=7&r=0&o=5&dpr=2&pid=1.7"/>
             <img src="https://th.bing.com/th/id/OIP.XQRY9e4L6UCn6Fhzm9TteQHaEo?w=275&h=180&c=7&r=0&o=5&dpr=2&pid=1.7"/>
           </div>
+          <a class="prev" onclick="plusSlides(-1)"></a>
+          <a class="next" onclick="plusSlides(1)"></a>
+        </div>
+        <div style="text-align:center">
+          <span class="dot" onclick="currentSlide(1)"></span> 
+          <span class="dot" onclick="currentSlide(2)"></span> 
+          <span class="dot" onclick="currentSlide(3)"></span> 
         </div>
         <h1>Welcome To The Futer</h1>
       </div>
