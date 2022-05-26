013cc632d89dcb7a8bbdb6bc46665cee28c61a58
kamryn0906 <delaleu.k@gmail.com>
2022-05-25 21:13:36 -0400

    updated header

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
         <p>
           Forecasting the future of technology is for dreamers who hope to innovate better tools and for the mainstream people who hope to benefit from the new and improved. 
           Many inventions are born in the lab and never make it into the consumer market, while others evolve beyond the pace of putting good regulations on their use.
-          Technology is ever-changing and ever-growing brings us the future we’ve always imagined.
+          Technology is ever-changing and ever-growing brings us the future we've always imagined.
         </p>
       </div>
     );
